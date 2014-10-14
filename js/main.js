a73cd134a30e91ec103b0ae6a0cedbcfaae5e90d
Valentin Ilie <eminemix@gmail.com>
2014-10-14 22:10:54 +0300

    Update with final result announcement on the main page.
    Signed-off-by: Valentin Ilie <eminemix@gmail.com>

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function ucShow(elem) {
 }
 
 function ucHideAll() {
-	var allElements = [acasa_content, regulament_content, proiecte_content, clasament_content, track_anul_1_content, track_anul_2_content, track_anul_3_content, track_anul_4_si_master_content];
+	var allElements = [acasa_content, regulament_content, proiecte_content, clasament_content, track_anul_1_content, track_anul_2_content, track_anul_3_content, track_anul_4_si_master_content, meta_content];
 	var allElementsLength = allElements.length;
 	for (var i = 0; i < allElementsLength; i++) {
 		allElements[i].style.display="none";
